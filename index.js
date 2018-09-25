11bb0d3bc66b93e66624da399244844ff350a8eb
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-09-25 07:33:41 +0300

    added first task

---
+++ index.js
@@ -0,0 +1,15 @@
+const args = process.argv.slice(2);
+
+switch (args[0]) {
+  case `--help`:
+    console.log(`This application does nothing. Accessible params:
+--help    — prints this info;
+--version — prints application version;`);
+    break;
+  case `--version`:
+    console.log(`v0.0.1`);
+    break;
+  default:
+    console.log(`This application doesn't anything helpful. Just prints some info and version.`);
+    console.error(`To list possible options use '--help'`);
+}
