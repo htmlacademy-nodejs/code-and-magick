27a0a745055a5bd8bfb1e111f97967c34c30417a
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-10-11 15:47:52 +0300

    host static from express

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@
 const express = require(`express`);
 const app = express();
 
-app.get(`/`, (req, res) => res.send(`Hello World!`));
+app.use(express.static(`static`));
 
 const runServer = (port) => {
 
