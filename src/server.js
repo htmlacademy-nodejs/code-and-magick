74cfbfff05162794310f01dff4dbec79689d8137
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-10-11 16:20:37 +0300

    add `/api/wizards` handler

--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
 'use strict';
 
 const express = require(`express`);
+const wizardsGenerator = require(`./generator/wizards-generator`);
 const app = express();
 
-app.use(express.static(`static`));
+app.use(express.static(`${__dirname}/../static`));
+
+const wizards = wizardsGenerator.generateEntity();
+
+app.get(`/api/wizards`, (req, res) => {
+  res.send(wizards);
+});
 
 const runServer = (port) => {
 
   port = parseInt(port, 10);
 
-  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+  app.listen(port, () => console.log(`Сервер запущен: http://localhost:${port}`));
 };
 
 const RUN_SERVER_COMMAND = `--server`;
@@ -22,3 +29,5 @@ module.exports = {
     runServer(port);
   }
 };
+
+runServer(3000);
