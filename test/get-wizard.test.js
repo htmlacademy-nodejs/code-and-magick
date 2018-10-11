05f91acbe0ade401b0f5e598059dbf8c80395954
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-10-11 16:50:29 +0300

    add test on unknown resource

--- test/get-wizard.test.js
+++ test/get-wizard.test.js
@@ -4,15 +4,24 @@ const assert = require(`assert`);
 const app = require(`../src/server`).app;
 
 describe(`GET /api/wizards`, () => {
-  it(`respond with json`, () => {
-    return request(app).
+  it(`get all wizards`, async () => {
+
+    const response = await request(app).
       get(`/api/wizards`).
       set(`Accept`, `application/json`).
       expect(200).
-      expect(`Content-Type`, /json/).
-      then((response) => {
-        const wizards = response.body;
-        assert.equal(wizards.length, 17);
-      });
+      expect(`Content-Type`, /json/);
+
+    const wizards = response.body;
+    assert.equal(wizards.length, 17);
   });
+
+  it(`get data from unknown resource`, async () => {
+    return request(app).
+      get(`/api/oneone`).
+      set(`Accept`, `application/json`).
+      expect(404).
+      expect(`Content-Type`, /html/);
+  });
+
 });
