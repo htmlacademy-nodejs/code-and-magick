092ecec4c0fa333ef0780ca343534d8803a38186
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-10-11 18:02:30 +0300

    post wizard test first

---
+++ test/post-wizard.test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const request = require(`supertest`);
+const assert = require(`assert`);
+
+const app = require(`../src/server`).app;
+
+describe(`POST /api/wizards`, () => {
+  it(`send wizard as json`, async () => {
+
+    const sent = {
+      name: `SuperWizard`
+    };
+
+    const response = await request(app).
+      post(`/api/wizards`).
+      send(sent).
+      set(`Accept`, `application/json`).
+      set(`Content-Type`, `application/json`).
+      expect(200).
+      expect(`Content-Type`, /json/);
+
+
+    const wizard = response.body;
+    assert.deepEqual(wizard, sent);
+  });
+
+});
+
