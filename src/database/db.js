112fa280f719526d8c3b5886508a0b35a25778f6
Johnny Rocket <0.snilcy@gmail.com>
2018-10-18 07:58:54 +0300

    Update db.js
    Для лечения:
    > (node:10468) DeprecationWarning: current URL string parser is deprecated, and will be removed in a future version. To use the new parser, pass option { useNewUrlParser: true } to MongoClient.connect.
    
    http://mongodb.github.io/node-mongodb-native/3.1/api/MongoClient.html

--- src/database/db.js
+++ src/database/db.js
@@ -4,7 +4,7 @@ const {MongoClient} = require(`mongodb`);
 
 const url = `mongodb://localhost:27017`;
 
-module.exports = MongoClient.connect(url).then((client) => client.db(`code-and-magick`)).catch((e) => {
+module.exports = MongoClient.connect(url, {useNewUrlParser: true}).then((client) => client.db(`code-and-magick`)).catch((e) => {
   console.error(`Failed to connect to MongoDB`, e);
   process.exit(1);
 });
